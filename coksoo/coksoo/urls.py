b5638cd1086f088ec31e7f53e68868ef7060e550
eldosagai <eldarasanbakov@mail.ru>
2023-12-08 00:10:36 +0600

    added swagger1

--- coksoo/coksoo/urls.py
+++ coksoo/coksoo/urls.py
@@ -68,6 +68,6 @@ urlpatterns = [
     # ), name='openapi-schema'),
     # path('openapi', schema_view, name='openapi-schema'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
