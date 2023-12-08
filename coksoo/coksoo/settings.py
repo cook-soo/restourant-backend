889c4288cf0a66f453ae7b4ce3cd461d88c54db8
eldosagai <eldarasanbakov@mail.ru>
2023-12-08 09:26:10 +0600

    https

--- coksoo/coksoo/settings.py
+++ coksoo/coksoo/settings.py
@@ -116,6 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SECURE_SSL_REDIRECT=False
+SESSION_COOKIE_SECURE=False
+CSRF_COOKIE_SECURE=False
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
